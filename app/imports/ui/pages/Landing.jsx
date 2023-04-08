0f3b3d7b3be2aac459fd4d4bb093fca845fcbf0c
justin_lisoway <jlisoway@hawaii.edu>
2023-04-08 06:26:25 -1000

    In progress Landing page 1

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Image, Row, Carousel } from 'react-bootstrap';
 import { PageIDs } from '../utilities/ids';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <div id={PageIDs.landingPage}>
     <div className="landing-green-background">
-      <Container className="text-center">
-        <h1 style={{ paddingTop: '20px', color: 'white', fontSize: '36pt' }}>
-          Welcome to Bowfolios
+      <Container>
+        <h1 style={{ paddingTop: '10px', paddingBottom: '15px', paddingLeft: '60px', color: 'white', fontSize: '50pt' }}>
+          <strong>Aloha Plants</strong>
+          <Image src="/images/flowers-1.png" width={100} paddingLeft={30} />
         </h1>
-        <h3 style={{ paddingBottom: '20px', color: 'white' }}>
-          Profiles, projects, and interest areas for the UH Community
-        </h3>
       </Container>
     </div>
-    <div className="landing-white-background">
+    <div className="landing-lightGreen-background">
       <Container className="justify-content-center text-center">
-        <h2 style={{ color: '#376551' }}>Start by making your profile....</h2>
-        <Row md={1} lg={2}>
-          <Col xs={6}>
-            <Image src="/images/home-page.png" width={500} />
+        <Row>
+          <Col xs={4}>
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="holder.js/800x400?text=First slide&bg=373940"
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
           </Col>
-          <Col xs={6}>
-            <Image src="/images/profiles-page.png" width={500} />
+          <Col xs={4}>
+            <Image src="/images/profiles-page.png" width={400} />
+          </Col>
+          <Col xs={4}>
+            <Image src="/images/profiles-page.png" width={400} />
           </Col>
         </Row>
       </Container>
