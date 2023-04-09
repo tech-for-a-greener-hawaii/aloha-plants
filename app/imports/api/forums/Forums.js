77af529c11e69949500b056469d9c2c934ab7700
justin_lisoway <jlisoway@hawaii.edu>
2023-04-09 06:54:11 -1000

    Add forums collection

---
+++ app/imports/api/forums/Forums.js
@@ -0,0 +1,26 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+/** Encapsulates state and variable values for this collection. */
+class ForumsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'ForumsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      name: { type: String, index: true, unique: true },
+      topic: { type: String, optional: true },
+      leadComment: { type: String, optional: true },
+      interests: { type: String, optional: true },
+    });
+    // Ensure collection documents obey schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+export const Forums = new ForumsCollection();
