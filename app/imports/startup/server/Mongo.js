63006ca050b43990b42267606416ce828c04bd65
jcnicolowUH <jnicolow@hawaii.edu>
2023-04-23 15:27:17 -1000

    debuging interests

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -80,8 +80,8 @@ if (Forums.collection.find().count() === 0) {
 /** Define a new forum. Error if project already exists.  */
 const addPlant = (plant) => {
   console.log(`Adding ${plant.name}`);
+  
   plant.interests.map(interest => PlantsInterests.collection.insert({ plantName: plant.name, interest }));
-  console.log(PlantsInterests.collection.find().count());
   Plants.collection.insert(plant);
 };
 
@@ -90,6 +90,7 @@ if (Plants.collection.find().count() === 0) {
     console.log('Creating default plants');
     // console.log(Meteor.settings.defaultPlants)
     Meteor.settings.defaultPlants.forEach(plant => addPlant(plant));
+    console.log(PlantsInterests.collection.find().count());
   }
 }
 
@@ -110,7 +111,8 @@ if (Meteor.users.find().count() === 0) {
     Meteor.settings.defaultProfiles.map(profile => addProfile(profile));
     console.log('Creating the default projects');
     Meteor.settings.defaultProjects.map(project => addProject(project));
-    // Meteor.settings.defaultPlants.map(plants => addPlant(plants));
+    console.log('Creating default plants');
+    Meteor.settings.defaultPlants.map(plants => addPlant(plants));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
