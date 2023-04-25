a5c36b690dd6defa5059e4b1227c1235a909dc5b
Austin Dang <austindang67@gmail.com>
2023-04-24 20:39:41 -1000

    Fixed autoform issue
    Can now add plants to database.

--- app/imports/ui/pages/AddPlant.jsx
+++ app/imports/ui/pages/AddPlant.jsx
@@ -1,75 +1,66 @@
 import React from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import { AutoForm, ErrorsField, LongTextField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import { AutoForm, ErrorsField, SubmitField, TextField } from 'uniforms-bootstrap5';
 import swal from 'sweetalert';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
 import { Plants } from '../../api/plants/Plants';
 
 // Create a schema to specify the structure of the data to appear in the form.
-const makeSchema = (allInterests) => new SimpleSchema({
+const formSchema = new SimpleSchema({
   name: String,
   scientificName: String,
+  indigenousStatus: String,
   description: String,
   growingConditions: String,
-  propagation: String,
+  propogation: String,
   range: String,
+  culturalUses: String,
   pests: String,
-  interests: { type: Array, label: 'Interests', optional: false },
-  'interests.$': { type: String, allowedValues: allInterests },
   picture: String,
 });
 
-const bridge = new SimpleSchema2Bridge(makeSchema);
+const bridge = new SimpleSchema2Bridge(formSchema);
 
-/* Renders the AddPlant page for adding a document. */
+/* Renders the Add Plant page for adding a plant. */
+
 const AddPlant = () => {
-
   // On submit, insert the data.
   const submit = (data, formRef) => {
-    const { name, scientificName, description, growingConditions, propagation, range, pests, interests, picture } = data;
+    const { name, scientificName, indigenousStatus, description, growingConditions, propogation, range, culturalUses, pests, picture } = data;
     Plants.collection.insert(
-      { name, scientificName, description, growingConditions, propagation, range, pests, interests, picture },
+      { name, scientificName, indigenousStatus, description, growingConditions, propogation, range, culturalUses, pests, picture },
       (error) => {
         if (error) {
           swal('Error', error.message, 'error');
         } else {
-          swal('Success', 'Item added successfully', 'success');
+          swal('Success', 'Plant added successfully', 'success');
           formRef.reset();
         }
       },
     );
   };
 
-  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
   let fRef = null;
   return (
     <Container className="py-3">
       <Row className="justify-content-center">
-        <Col xs={10}>
-          <Col className="text-center"><h2>Add Forum</h2></Col>
+        <Col xs={5}>
+          <Col className="text-center"><h2>Add Plants</h2></Col>
           <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
             <Card>
               <Card.Body>
-                <Row>
-                  <Col>
-                    <TextField name="name" />
-                    <TextField name="scientificName" />
-                    <TextField name="description" />
-                    <TextField name="growingConditions" />
-                    <TextField name="propagation" />
-                    <TextField name="range" />
-                    <TextField name="pests" />
-                    <TextField name="interest" />
-                    <TextField name="picture" />
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
-                    <LongTextField name="leadComment" />
-                  </Col>
-                  <SubmitField value="Submit" />
-                </Row>
+                <TextField name="name" />
+                <TextField name="scientificName" />
+                <TextField name="indigenousStatus" />
+                <TextField name="description" />
+                <TextField name="growingConditions" />
+                <TextField name="propogation" />
+                <TextField name="range" />
+                <TextField name="culturalUses" />
+                <TextField name="pests" />
+                <TextField name="picture" />
+                <SubmitField value="Submit" />
                 <ErrorsField />
               </Card.Body>
             </Card>
