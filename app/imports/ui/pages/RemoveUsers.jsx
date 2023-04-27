02db696dedd4fa2d8d59dfdb22406e04c93ce097
Brian Dang <30878065+Brian-D-Dang@users.noreply.github.com>
2023-04-27 12:43:56 -1000

    added type popup to confirm user delete

--- app/imports/ui/pages/RemoveUsers.jsx
+++ app/imports/ui/pages/RemoveUsers.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Col, Image, Row, Modal, InputGroup, Form } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
 import { useTracker } from 'meteor/react-meteor-data';
@@ -10,6 +10,14 @@ import LoadingSpinner from '../components/LoadingSpinner';
 /* A simple static component to render some text for the landing page. */
 
 const ProfileCard = ({ profile }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const remove = (data) => {
+    Profiles.collection.remove({ _id: data._id });
+  };
+
   const defaultProfileImage = '/images/defaultProfileImage.png';
   const checkProfileImage = profile.picture != null ? profile.picture : defaultProfileImage;
   return (
@@ -17,6 +25,30 @@ const ProfileCard = ({ profile }) => {
       <p>{profile.email}</p>
       <p>{profile.firstName} {profile.lastName}</p>
       <Image roundedCircle src={checkProfileImage} width={50} />
+      <Button
+        className="mt-auto"
+        variant="danger"
+        onClick={handleShow}
+      >
+        Leave
+      </Button>
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Remove User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Type DELETE to confirm</Modal.Body>
+        <Modal.Footer>
+          <Form.Control
+            aria-label="Username"
+          />
+          {/*<Button variant="secondary" onClick={handleClose}>*/}
+          {/*  Close*/}
+          {/*</Button>*/}
+          {/*<Button variant="primary" onClick={handleClose}>*/}
+          {/*  Save Changes*/}
+          {/*</Button>*/}
+        </Modal.Footer>
+      </Modal>
     </Col>
   );
 };
