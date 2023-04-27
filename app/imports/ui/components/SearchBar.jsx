90614b029bda5ec61d363bf4b892a0ca83f4fb72
burkse01 <burkse@hawaii.edu>
2023-04-26 22:02:53 -1000

    Update SearchBar.jsx

--- app/imports/ui/components/SearchBar.jsx
+++ app/imports/ui/components/SearchBar.jsx
@@ -1,28 +1,31 @@
-import React, {useState} from 'react';
-import { Col, Container, Row, Spinner } from 'react-bootstrap';
-import { TextField } from 'uniforms-bootstrap5';
-//baseData = is the data retrieved from the server and is not changed
-//filteredDataSetter is a function to modify the data that is being changed
-//dataFilterFunction is the function used to filter the data while searching
-const SearchBar = ({filteredData, baseData, filteredDataSetter, dataFilterFunction}) => {
-  const [searchIn, setSearchIn] = useState("");
-  if(filteredData == undefined || (filteredData.length != baseData.length && searchIn.length == 0)) {
-    filteredDataSetter(baseData)
-  }
+import React, { useState } from 'react';
+import { Container, Row } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+const SearchBar = ({ filteredData, baseData, filteredDataSetter, dataFilterFunction }) => {
+  SearchBar.propTypes = {
+    filteredData: PropTypes.PropTypes.objectOf(PropTypes.object()).isRequired,
+    baseData: PropTypes.PropTypes.objectOf(PropTypes.object()).isRequired,
+    filteredDataSetter: PropTypes.func.isRequired,
+    dataFilterFunction: PropTypes.func.isRequired,
+  };
+  const [searchIn, setSearchIn] = useState('');
+  if (filteredData === undefined || (filteredData.length !== baseData.length && searchIn.length === 0)) { filteredDataSetter(baseData); }
   function filterFunctionWrapper(input) {
     return dataFilterFunction(input, searchIn);
   }
   const handleInput = (e) => {
     setSearchIn(e.target.value);
     filteredDataSetter(baseData.filter(filterFunctionWrapper));
-  }
+  };
   return (
     <Container>
       <Row className="justify-content-md-center">
         <div>
-          <input type="search"
-                 placeholder="Search"
-                 onChange={handleInput}
+          <input
+            type="search"
+            placeholder="Search"
+            onChange={handleInput}
           />
         </div>
       </Row>
