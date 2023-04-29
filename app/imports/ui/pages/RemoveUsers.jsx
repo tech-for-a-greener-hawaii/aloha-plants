dab6367e057e5da7dd710096b85a0b73f0d636fc
Brian Dang <30878065+Brian-D-Dang@users.noreply.github.com>
2023-04-28 14:23:30 -1000

    finished creating new user cards and delete confirmation will add in reactive state to these cards

--- app/imports/ui/pages/RemoveUsers.jsx
+++ app/imports/ui/pages/RemoveUsers.jsx
@@ -1,54 +1,70 @@
 import React, { useState } from 'react';
-import { Button, Col, Image, Row, Modal, InputGroup, Form } from 'react-bootstrap';
+import { Button, Col, Image, Row, Modal, InputGroup, Form, Card, Badge } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
 import { useTracker } from 'meteor/react-meteor-data';
-import { PageIDs } from '../utilities/ids';
+import { ComponentIDs, PageIDs } from '../utilities/ids';
 import { Profiles } from '../../api/profiles/Profiles';
 import LoadingSpinner from '../components/LoadingSpinner';
+import { AutoForm, ErrorsField, SubmitField, TextField } from 'uniforms-bootstrap5';
 
 /* A simple static component to render some text for the landing page. */
 
 const ProfileCard = ({ profile }) => {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const input = document.getElementById('deleteText');
   const remove = (data) => {
-    Profiles.collection.remove({ _id: data._id });
+    if (input.value === 'DELETE') {
+      Profiles.collection.remove({ _id: data._id });
+      setShow(false);
+      // eslint-disable-next-line no-restricted-globals
+      location.reload();
+    }
   };
 
   const defaultProfileImage = '/images/defaultProfileImage.png';
   const checkProfileImage = profile.picture != null ? profile.picture : defaultProfileImage;
   return (
     <Col>
-      <p>{profile.email}</p>
-      <p>{profile.firstName} {profile.lastName}</p>
-      <Image roundedCircle src={checkProfileImage} width={50} />
-      <Button
-        className="mt-auto"
-        variant="danger"
-        onClick={handleShow}
-      >
-        Leave
-      </Button>
-      <Modal show={show} onHide={handleClose} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>Remove User</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Type DELETE to confirm</Modal.Body>
-        <Modal.Footer>
-          <Form.Control
-            aria-label="Username"
-          />
-          {/*<Button variant="secondary" onClick={handleClose}>*/}
-          {/*  Close*/}
-          {/*</Button>*/}
-          {/*<Button variant="primary" onClick={handleClose}>*/}
-          {/*  Save Changes*/}
-          {/*</Button>*/}
-        </Modal.Footer>
-      </Modal>
+      <Card className="h-100 center-text" id="project-card">
+        <Card.Body>
+          <Card.Img src={checkProfileImage} width={50} />
+          <Card.Title style={{ marginTop: '10px', textAlign: 'center' }}>
+            <p>{profile.firstName} {profile.lastName}</p>
+          </Card.Title>
+          <Card.Text style={{ textAlign: 'center' }}>
+            <p>{profile.email}</p>
+          </Card.Text>
+        </Card.Body>
+        <Button
+          className="mt-auto"
+          variant="danger"
+          onClick={handleShow}
+        >
+          Delete
+        </Button>
+        <Modal show={show} onHide={handleClose} centered>
+          <Modal.Header closeButton>
+            <Modal.Title>Remove User</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Type DELETE to confirm</p>
+            <Form.Control
+              id="deleteText"
+              aria-label="Username"
+            />
+            <div className="d-flex justify-content-end mt-3">
+              <Button id="deleteButton" variant="danger" onClick={() => remove(profile)}>
+                CONFIRM
+              </Button>
+            </div>
+
+          </Modal.Body>
+        </Modal>
+      </Card>
     </Col>
   );
 };
@@ -72,7 +88,7 @@ const RemoveUsers = () => {
   const profiles = Profiles.collection.find().fetch();
   return ready ? (
     <div id={PageIDs.removeUsersPage}>
-      <Row xs={1} md={2} lg={4} className="g-2 mt-2">
+      <Row xs={1} md={2} lg={6} className="g-2 mt-2">
         {profiles.map((profile, index) => <ProfileCard key={index} profile={profile} />)}
       </Row>
     </div>
