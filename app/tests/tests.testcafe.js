da0f07b188b2b3f283bee3f2a06837b2d210dc13
burkse01 <burkse@hawaii.edu>
2023-05-01 04:34:31 -1000

    Update tests.testcafe.js

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -10,11 +10,15 @@ import { homePage } from './home.page';
 import { addProjectPage } from './addproject.page';
 import { filterPage } from './filter.page';
 import { navBar } from './navbar.component';
+import { forumsPage} from './forums.page';
+import { addPlantPage} from './addplant.page';
+import { plantsPage} from './plants.page';
+import { editProjectPage } from './editproject.page';
 
 /** Credentials for one of the sample users defined in settings.development.json. */
 const credentials = { username: 'admin@foo.com', password: 'foo', firstName: 'Philip', lastName: 'Johnson' };
 
-fixture('Bowfolios localhost test with default db')
+fixture('Aloha PLants localhost test with default db')
   .page('http://localhost:3000');
 
 test('Test that landing page shows up', async (testController) => {
@@ -41,17 +45,8 @@ test('Test that signup page, then logout works', async (testController) => {
 test('Test that projects page displays', async (testController) => {
   await navBar.gotoProjectsPage(testController);
   await projectsPage.isDisplayed(testController);
-  //await projectsPage.hasDefaultProjects(testController);
 });
 
-test('Test that home page display and profile modification works', async (testController) => {
-  await navBar.ensureLogout(testController);
-  await navBar.gotoSignInPage(testController);
-  await signInPage.signin(testController, credentials.username, credentials.password);
-  await homePage.isDisplayed(testController);
-  //await homePage.updateProfile(testController, credentials.firstName);
-  await navBar.ensureLogout(testController);
-});
 
 test('Test that addProject page works', async (testController) => {
   await navBar.gotoSignInPage(testController);
@@ -64,10 +59,8 @@ test('Test that projects page displays', async (testController) => {
   await navBar.gotoProjectsPage(testController);
   await projectsPage.isDisplayed(testController);
 });
+
 test('Test that plants page displays', async (testController) => {
-  await navBar.ensureLogout(testController);
-  await navBar.gotoSignInPage(testController);
-  await signInPage.signin(testController, credentials.username, credentials.password);
   await navBar.gotoPlantsPage(testController);
   await plantsPage.isDisplayed(testController);
 });
@@ -78,6 +71,7 @@ test('Test that forums page displays', async (testController) => {
   await navBar.gotoForumsPage(testController);
   await forumsPage.isDisplayed(testController);
 });
+
 test('Test that addplant page displays', async (testController) => {
   await navBar.ensureLogout(testController);
   await navBar.gotoSignInPage(testController);
