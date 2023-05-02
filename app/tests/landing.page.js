3a5da2583a7628946d53f3275343d974e69ebc65
burkse01 <burkse@hawaii.edu>
2023-05-01 17:49:56 -1000

    Update landing.page.js

--- app/tests/landing.page.js
+++ app/tests/landing.page.js
@@ -10,7 +10,7 @@ class LandingPage {
   /** Asserts that this page is currently displayed. */
   async isDisplayed(testController) {
     // This is first test to be run. Wait 20 seconds to avoid timeouts with GitHub Actions.
-    await testController.wait(20000).expect(this.pageSelector.exists).ok();
+    await testController.expect(this.pageSelector.exists).ok();
   }
 }
 
